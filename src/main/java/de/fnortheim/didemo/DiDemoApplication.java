8fced47a4ec07465126e01284cf2d81da599bcef
Sebastian Basner <sebastian.basner@gmx.de>
2019-03-17 22:09:34 +0100

    Added sample to manually get bean from ApplicationContext without using 'new'

--- src/main/java/de/fnortheim/didemo/DiDemoApplication.java
+++ src/main/java/de/fnortheim/didemo/DiDemoApplication.java
@@ -1,13 +1,20 @@
 package de.fnortheim.didemo;
 
+import de.fnortheim.didemo.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiDemoApplication.class, args);
+        final ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+
+        // Bean with @Controller annotation is createg by Spring context and can be used without manually creating a new instance
+        MyController myController = (MyController) ctx.getBean("myController");
+        myController.hello();
     }
 
 }
---
+++ src/main/java/de/fnortheim/didemo/controllers/MyController.java
@@ -0,0 +1,14 @@
+package de.fnortheim.didemo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * created by sebastianbasner on Mar, 2019
+ */
+@Controller
+public class MyController {
+
+    public void hello() {
+        System.out.println("Hello World");
+    }
+}
